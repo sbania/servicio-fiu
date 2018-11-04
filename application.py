f5fecc67d54ac88df29c87bad4be370c606f8090
sbania <sbania@gmail.com>
2018-11-04 17:47:32 -0500

    agregue metodo registrar

--- application.py
+++ application.py
@@ -2,5 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def echo():
+    return "El servicio FIU esta activo!"
+
+@app.route('/registrar', methods=['POST']) 
+def registrar():
+    if not request.json:
+        abort(400)
+    print request.json
+    return json.dumps(request.json)
